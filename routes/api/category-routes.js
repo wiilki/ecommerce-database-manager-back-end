942af3a26ad40294a87702a3a2f612ec3789e47c
Will <willkim0608@gmail.com>
2023-01-17 13:36:09 -0800

    Edit rest of functions in category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,7 +5,6 @@ const { Category, Product } = require('../../models');
 
 router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
   try {
     const seedCategories = await Category.findAll({
       include: [{ model: Product }],
@@ -16,21 +15,70 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const seedCategories = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!seedCategories) {
+      res.status(404).json({ message: 'No reader found with that id!' });
+      return;
+    }
+
+    res.status(200).json(seedCategories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const seedCategories = await Category.create(req.body);
+    res.status(200).json(seedCategories);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedBook) => {
+      res.json(updatedBook);
+    })
+    .catch((err) => res.json(err));
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const seedCategories = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!seedCategories) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(seedCategories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
